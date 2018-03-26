57333dfaf46c07322ededc12f05aa73103a3cb69
hshiva <shivakumara.ckm@gmail.com>
2018-03-26 08:39:33 +0530

    checked in code

---
+++ BookFlight/BookFlight.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TravelRepublic.FlightCodingTest;
+
+namespace BookFlight
+{
+    static class BookFlight
+    {
+        static void Main()
+        {
+            GetFlightDetails();
+            //TODO
+            //Book Flight
+        }
+
+        private static void GetFlightDetails()
+        {
+            FlightBuilder filghtInfo = FlightBuilder.GetIntance;
+            var flightDetails = filghtInfo.GetFlights();
+            foreach (var flight in flightDetails)
+            {
+                foreach (var segments in flight.Segments)
+                {
+                    Console.WriteLine(
+                        $"flight Details : Departure Date and Time = {segments.DepartureDate}, Arrival Date and Time ={segments.ArrivalDate} ");
+                }
+                Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TravelRepublic.FlightCodingTest/FlightBuilder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TravelRepublic.FlightCodingTest
+{
+    public sealed class FlightBuilder
+    {
+        private DateTime _threeDaysFromNow;
+        private int noOfdays = 3;
+        private FlightBuilder()
+        {
+            _threeDaysFromNow = DateTime.Now.AddDays(noOfdays);
+        }
+        private static readonly Lazy<FlightBuilder> Intance = new Lazy<FlightBuilder>(() => new FlightBuilder());
+
+        public static FlightBuilder GetIntance => Intance.Value;
+
+        public IList<Flight> GetFlights()
+        {
+            return new List<Flight>
+            {
+                     //A normal flight with two hour duration
+                     CreateFlight(_threeDaysFromNow, _threeDaysFromNow.AddHours(2)),
+
+                     //A normal multi segment flight
+                     CreateFlight(_threeDaysFromNow, _threeDaysFromNow.AddHours(2), _threeDaysFromNow.AddHours(3), _threeDaysFromNow.AddHours(5)),
+
+                     //A flight departing in the past
+                     CreateFlight(_threeDaysFromNow.AddDays(-6), _threeDaysFromNow),
+
+                     //A flight that departs before it arrives
+                     CreateFlight(_threeDaysFromNow, _threeDaysFromNow.AddHours(-6)),
+
+                     //A flight with more than two hours ground time
+                     CreateFlight(_threeDaysFromNow, _threeDaysFromNow.AddHours(2), _threeDaysFromNow.AddHours(5), _threeDaysFromNow.AddHours(6)),
+
+                     //Another flight with more than two hours ground time
+                     CreateFlight(_threeDaysFromNow, _threeDaysFromNow.AddHours(2), _threeDaysFromNow.AddHours(3), _threeDaysFromNow.AddHours(4),
+                     _threeDaysFromNow.AddHours(6), _threeDaysFromNow.AddHours(7)),
+
+                    
+                      //1.Derpart before current date
+                       CreateFlight(_threeDaysFromNow.AddDays(-3),_threeDaysFromNow.AddDays(-noOfdays)),
+
+                      //2. Have a segment with an arrival date before the departure date.
+                      CreateFlight(_threeDaysFromNow.AddHours(6),_threeDaysFromNow),
+
+                      //3. Spend more than 2 hours on the ground. i.e those with a total gap of over two hours between the arrival date of one segment and 
+                      //the departure date of the next.
+                      CreateFlight(_threeDaysFromNow.AddDays(-noOfdays),_threeDaysFromNow.AddDays(-noOfdays).AddHours(2),_threeDaysFromNow.AddDays(-noOfdays).AddHours(5),_threeDaysFromNow.AddDays(-noOfdays).AddHours(6))
+
+             };
+        }
+
+        private static Flight CreateFlight(params DateTime[] dates)
+        {
+            if (dates.Length % 2 != 0) throw new ArgumentException($"You must pass an even number of dates,{dates}");
+
+            var departureDates = dates.Where((date, index) => index % 2 == 0);
+            var arrivalDates = dates.Where((date, index) => index % 2 == 1);
+
+            var segments = departureDates.Zip(arrivalDates,
+                                              (departureDate, arrivalDate) =>
+                                              new Segment { DepartureDate = departureDate, ArrivalDate = arrivalDate }).ToList();
+
+            return new Flight { Segments = segments };
+        }
+    }
+
+    public class Flight
+    {
+        public IList<Segment> Segments { get; set; }
+    }
+
+    public class Segment
+    {
+        public DateTime? DepartureDate { get; set; }
+        public DateTime? ArrivalDate { get; set; }
+    }
+}
+
